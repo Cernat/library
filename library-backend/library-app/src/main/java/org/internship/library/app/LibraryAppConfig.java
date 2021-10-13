f5b9ab9a67da6771c2b72fbdd61df0d70deea55e
Cernat <cernatrazvanm@yahoo.com>
2021-10-13 18:00:58 +0300

    Created LibraryAppConfig Removed 1 Swagger Dependency #5 Removed * imports Moved LibraryApplication Bean

---
+++ library-backend/library-app/src/main/java/org/internship/library/app/LibraryAppConfig.java
@@ -0,0 +1,30 @@
+package org.internship.library.app;
+
+import org.internship.library.api.BookService;
+import org.internship.library.impl.BookServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class LibraryAppConfig {
+
+    @Bean
+    public BookService bookService() {
+        return new BookServiceImpl();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
--- library-backend/library-app/src/main/java/org/internship/library/app/LibraryApplication.java
+++ library-backend/library-app/src/main/java/org/internship/library/app/LibraryApplication.java
@@ -1,36 +1,14 @@
 package org.internship.library.app;
 
-import org.internship.library.api.BookService;
 import org.springframework.boot.SpringApplication;
-import org.internship.library.impl.BookServiceImpl;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
-	@Bean
-	public BookService bookService() {
-		return new BookServiceImpl();
-	}
 
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build();
-	}
 }
