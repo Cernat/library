bba96b90f3f8c536313059dc0cae0edcecbda681
Cernat <cernatrazvanm@yahoo.com>
2021-10-15 14:19:38 +0300

    Required changes made.

--- spring-learning/src/main/java/spring/learning/video/three/Triangle.java
+++ spring-learning/src/main/java/spring/learning/video/three/Triangle.java
@@ -4,20 +4,19 @@ import java.util.List;
 
 public class Triangle {
 
+    private List<Point> points;
+
+    public void draw() {
+        for (Point point : points) {
+            System.out.println("Point = (" + point.getX() +", " + point.getY() +")");
+        }
+    }
+
     public List<Point> getPoints() {
         return points;
     }
 
     public void setPoints(List<Point> points) {
         this.points = points;
-    }
-
-    private List<Point> points;
-
-    public void draw() {
-
-        for (Point point : points) {
-        System.out.println("Point = (" + point.getX() +", " + point.getY() +")");
-        }
     }
 }
