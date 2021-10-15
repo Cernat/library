65b2dfb45718112a3b881bca893a9453fb4e677a
Cernat <38557075+Cernat@users.noreply.github.com>
2021-10-15 12:37:00 +0300

    Constructor-arg tag to intialize value (Constructor Injection) (#10)
    * Constructor-arg tag to intializa value (Constructor Injection)
    Propriety tag is using setter to intialize value (Setter Injection)
    We can have as many constructor as we want
    Spring converts arguments for constructor
    Type propriety tells the type of the constructor argument
    Index propriety tells the order of the constructor argument
    
    * White spaces eliminated

--- spring-learning/src/main/java/spring/learning/video/three/Triangle.java
+++ spring-learning/src/main/java/spring/learning/video/three/Triangle.java
@@ -2,16 +2,36 @@ package spring.learning.video.three;
 
 public class Triangle {
 
+    private String type;
+    private int height;
+
+    public Triangle(String type) {
+        this.type = type;
+    }
+
+    public Triangle(int height) {
+        this.height = height;
+    }
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+
+    public int getHeight() {
+        return height;
     }
 
-    public String type;
+
     public void draw() {
-        System.out.println( getType() + " Triangle Drawn");
+        System.out.println(getType() + " Triangle Drawn of height " + getHeight());
     }
 }
