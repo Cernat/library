7c0919f65d85e1d1248c6665656018c427345c47
Cernat <cernatrazvanm@yahoo.com>
2021-10-18 10:11:17 +0300

    ApplicationContextAware Why? When you need ApplicationContext inside a class Creates method setApplicationContext(inside Triangle class) Interface BeanNameAware- set the name of the bean. #7

--- spring-learning/src/main/java/spring/learning/video/three/Triangle.java
+++ spring-learning/src/main/java/spring/learning/video/three/Triangle.java
@@ -1,12 +1,18 @@
 package spring.learning.video.three;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 import java.util.List;
 
-public class Triangle {
+public class Triangle implements ApplicationContextAware, BeanNameAware {
 
     private Point pointA;
     private Point pointB;
     private Point pointC;
+    private ApplicationContext context = null;
 
     public Point getPointA() {
         return pointA;
@@ -38,4 +44,14 @@ public class Triangle {
         System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext context)
+            throws BeansException {
+        this.context = context;
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("Bean name is: " + beanName);
+    }
 }
