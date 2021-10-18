300285622c5f6354d4996788d8c285b0eb1c94ad
Cernat <cernatrazvanm@yahoo.com>
2021-10-18 13:51:42 +0300

    JSR-250 Annotations @Resource(name="pointC") - injection by name Let us specify a bean by name Inject dependencies into a member variable of class If we don't specify name, Spring is looking for bean with id of same name ("center") @PostConstruct, @PreDestroy Specify initialization and destruction methods for our beans #7

--- spring-learning/src/main/java/spring/learning/video/three/Circle.java
+++ spring-learning/src/main/java/spring/learning/video/three/Circle.java
@@ -1,7 +1,8 @@
 package spring.learning.video.three;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 
 public class Circle implements Shape {
 
@@ -17,9 +18,18 @@ public class Circle implements Shape {
         return center;
     }
 
-    @Autowired
-    @Qualifier("circleRelated")
+    @Resource(name = "pointC")
     public void setCenter(Point center) {
         this.center = center;
+    }
+
+    @PostConstruct
+    public void initializeCircle() {
+        System.out.println("Init of Circle");
+    }
+
+    @PreDestroy
+    public void destroyCircle() {
+        System.out.println("Destroy of Circle");
     }
 }
--- spring-learning/src/main/java/spring/learning/video/three/DrawingApp.java
+++ spring-learning/src/main/java/spring/learning/video/three/DrawingApp.java
@@ -8,7 +8,8 @@ public class DrawingApp {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        context.registerShutdownHook();
         Shape shape = (Shape) context.getBean("circle");
         shape.draw();
     }
