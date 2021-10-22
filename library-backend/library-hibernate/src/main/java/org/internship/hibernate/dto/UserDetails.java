330acdf3e687428fcd29ef623e2fa328aa4c17a5
Cernat <38557075+Cernat@users.noreply.github.com>
2021-10-22 11:09:38 +0300

    hbm2ddl Configuration and Name Annotations (#41)
    hbm2ddl.auto - when set to create
    Drop and re-create the database schema on startup
    	     - when set to update
    It only makes a change if it finds a change
    "show_sql" set to true - show in the console sql queries executed
    @Column
    We can put @Id and @Column above getter methods

--- library-backend/library-hibernate/src/main/java/org/internship/hibernate/dto/UserDetails.java
+++ library-backend/library-hibernate/src/main/java/org/internship/hibernate/dto/UserDetails.java
@@ -2,14 +2,17 @@ package org.internship.hibernate.dto;
 
 import org.hibernate.annotations.Table;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name = "USER_DETAILS")
 public class UserDetails {
 
     @Id
+    @Column(name="USER_ID")
     private int userId;
+    @Column(name="USER_NAME")
     private String userName;
 
     public int getUserId() {
