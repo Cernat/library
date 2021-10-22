bb386df9f4e190df8d522484f193790e1af6a95f
Cernat <38557075+Cernat@users.noreply.github.com>
2021-10-22 12:26:59 +0300

    More Annotations (#42)
    * More Annotations
    @Table - used to set the table name
    Difference between @Entity(name="") and @Table(name="")
    @Table(name="") provides a name for the table which is created for the entity
    @Basic - treat it as a field that should persisted
    Transient or static properties, hibernate will automatically not persist
    @Transient - hibernate will ignore this field
    @Temporal - used for specifing the date type variable(TemporalType.DATE or TemporalType.TIMESTAMP)
    @Lob - Allow us to have a large object (we are not anymore restricted by var(255) from DB)
    
    * Maven Description, Javadoc Added

--- library-backend/library-hibernate/src/main/java/org/internship/hibernate/test/HibernateTest.java
+++ library-backend/library-hibernate/src/main/java/org/internship/hibernate/test/HibernateTest.java
@@ -5,12 +5,25 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.internship.hibernate.dto.UserDetails;
 
+import java.util.Date;
+
+/**
+ * Class used for creating and manipulate data using Hibernate
+ */
 public class HibernateTest {
     public static void main(String[] args) {
         UserDetails user = new UserDetails();
         user.setUserId(1);
         user.setUserName("First User");
+        user.setAddress("Address 1");
+        user.setJoinedDate(new Date());
+        user.setDescription("Description 1");
 
+        /**
+         * .configure() - uses the hibernate.cfg.xml configuration file
+         * .buildSessionFactory() - builds a session
+         * .openSession() - open a session that allows us to make transactions
+         */
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
