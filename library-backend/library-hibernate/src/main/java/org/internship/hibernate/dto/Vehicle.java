0bd40983646c63510f3c0051f9f089a1368514db
Cernat <38557075+Cernat@users.noreply.github.com>
2021-10-25 11:51:17 +0300

    Implementing Inheritance With Table Per Class Strategy (#55)
    Table Per Class Strategy - We're going to have a separate table for each of the classes that we need to persist
    The parent class properties are inherited and they form separate columns even in the child tables
    The generated value(id) of the parent class is inherited in the child class and hibernate generate the column automatically
    Doesn't implement blank column from others child properties
    
    #28

--- library-backend/library-hibernate/src/main/java/org/internship/hibernate/dto/Vehicle.java
+++ library-backend/library-hibernate/src/main/java/org/internship/hibernate/dto/Vehicle.java
@@ -20,11 +20,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(
-        name="VEHICLE_TYPE",
-        discriminatorType = DiscriminatorType.STRING
-)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Vehicle {
 
     @Id
