b33d7e0cd8afb0a2c91e00169f141518b58cdf9a
Cernat <cernatrazvanm@yahoo.com>
2021-10-25 15:27:25 +0300

    Understanding Parameter Binding and SQL Injection
    Parameter Binding - it's a way in which you can bind specific values to parameters in sequel query
    Query query = session.createQuery("from UserDetails where userId > " + minUserId); - it could be a SQL Injection if String minUserId = " 5 or 1 = 1";
    
    Query query = session.createQuery("from UserDetails where userId > ?"); - ERROR: Legacy-style query parameters (`?`) are no longer supported; use JPA-style ordinal parameters (e.g., `?1`)
    query.setInteger(0, Integer.parseInt(minUserId));
    
    Query query = session.createQuery("from UserDetails where userId > :userId and  userName = :userName");
    query.setInteger("userId", Integer.parseInt(minUserId));
    query.setString("userName", userName);
    
    It is recommended that we use parameter substitution for any queries parameter because appending parameters could might lead to sequel injection.
    
    #28

--- library-backend/library-hibernate/src/main/java/org/internship/hibernate/test/HibernateTest.java
+++ library-backend/library-hibernate/src/main/java/org/internship/hibernate/test/HibernateTest.java
@@ -1,5 +1,6 @@
 package org.internship.hibernate.test;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -8,14 +9,13 @@ import org.internship.hibernate.dto.TwoWheeler;
 import org.internship.hibernate.dto.UserDetails;
 import org.internship.hibernate.dto.Vehicle;
 
+import java.util.List;
+
 /**
  * Class used for creating and manipulate data using Hibernate
  */
 public class HibernateTest {
     public static void main(String[] args) {
-
-        UserDetails user = new UserDetails();
-        user.setUserName("Test User");
 
         /**
          * .configure() - uses the hibernate.cfg.xml configuration file
@@ -25,15 +25,22 @@ public class HibernateTest {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
+//        String minUserId = " 5 or 1 = 1";
+        String minUserId = "5";
+        String userName = "User 9";
 
-        session.save(user);
+//        Query query = session.createQuery("from UserDetails where userId > " + minUserId);
+        Query query = session.createQuery("from UserDetails where userId > :userId and  userName = :userName");
+        query.setInteger("userId", Integer.parseInt(minUserId));
+        query.setString("userName", userName);
 
-        user.setUserName("Updated User");
-        user.setUserName("Updated User Again");
+        List<UserDetails> users = (List<UserDetails>) query.list();
 
         session.getTransaction().commit();
         session.close();
 
-        user.setUserName("Updated User After Session Close");
+        for (UserDetails user : users)
+            System.out.println(user.getUserName());
+
     }
 }
