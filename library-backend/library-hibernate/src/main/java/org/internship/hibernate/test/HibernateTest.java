ffe1913231151b6aa4110f8e3c2d2ba485e13dd3
Cernat <cernatrazvanm@yahoo.com>
2021-10-25 17:01:55 +0300

    Projections and Query By Example
    Criteria criteria = session.createCriteria(UserDetails.class)
                    .setProjection(Projections.property("userId")); - set projection for whatever data that I need
    It's going to return me just the userId
    Projection.max("userId"); - return max
    Return all the values of a particular column - it gives you the aggregate method
    The returned object it's not going to be a list of users anymore.
    
    Querying by Example - handy way if you have too many properties and too many criteria to satisfy
    Create an object and automatically make the criteria based on this object.
    Tell hibernate that I am interested in creating an example out of this object
    
    Example example = Example.create(exampleUser);
    e.g Criteria criteria = session.createCriteria(UserDetails.class)
                    .add(example);
    
    #28

--- library-backend/library-hibernate/src/main/java/org/internship/hibernate/test/HibernateTest.java
+++ library-backend/library-hibernate/src/main/java/org/internship/hibernate/test/HibernateTest.java
@@ -1,21 +1,21 @@
 package org.internship.hibernate.test;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.internship.hibernate.dto.FourWheeler;
-import org.internship.hibernate.dto.TwoWheeler;
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 import org.internship.hibernate.dto.UserDetails;
-import org.internship.hibernate.dto.Vehicle;
+
+import java.util.List;
 
 /**
  * Class used for creating and manipulate data using Hibernate
  */
 public class HibernateTest {
     public static void main(String[] args) {
-
-        UserDetails user = new UserDetails();
-        user.setUserName("Test User");
 
         /**
          * .configure() - uses the hibernate.cfg.xml configuration file
@@ -26,14 +26,27 @@ public class HibernateTest {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        session.save(user);
+        UserDetails exampleUser = new UserDetails();
+//        exampleUser.setUserId(5);
+//        exampleUser.setUserName("User 5");
+        exampleUser.setUserName("User 1%");
 
-        user.setUserName("Updated User");
-        user.setUserName("Updated User Again");
+//        Example example = Example.create(exampleUser).excludeProperty("userName");
+        Example example = Example.create(exampleUser).enableLike();
+
+        Criteria criteria = session.createCriteria(UserDetails.class)
+                .add(example);
+//                .setProjection(Projections.property("userId"));
+//                .addOrder(Order.desc("userId"));
+
+        List<UserDetails> users = (List<UserDetails>) criteria.list();
 
         session.getTransaction().commit();
         session.close();
 
-        user.setUserName("Updated User After Session Close");
+
+        for (UserDetails user : users)
+            System.out.println(user.getUserName());
+
     }
 }
