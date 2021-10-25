c4e899273aaf535fe355011391a50bb72cb5b764
Cernat <38557075+Cernat@users.noreply.github.com>
2021-10-25 17:25:38 +0300

    Update HibernateTest.java

--- library-backend/library-hibernate/src/main/java/org/internship/hibernate/test/HibernateTest.java
+++ library-backend/library-hibernate/src/main/java/org/internship/hibernate/test/HibernateTest.java
@@ -32,7 +32,7 @@ public class HibernateTest {
         Session session2 = sessionFactory.openSession();
         session2.beginTransaction();
 
-        UserDetails user2 = (UserDetails) session.get(UserDetails.class, 1);
+        UserDetails user2 = (UserDetails) session2.get(UserDetails.class, 1);
 
         session2.getTransaction().commit();
         session2.close();
