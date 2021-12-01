75c8c576984a81cc79a40f14b1882dba8b146d9a
Cernat <cernatrazvanm@yahoo.com>
2021-12-01 12:25:35 +0200

    Displaying Data and Handling Events Binding, Bootstrap, Events, Pipe

--- library-frontend/src/app/app.module.ts
+++ library-frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SummaryPipe } from './summary.pipe';
 import { CourseComponent } from './course/course.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,15 +6,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CoursesService } from './courses.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    SummaryPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     CoursesService
--- library-frontend/src/app/courses.component.ts
+++ library-frontend/src/app/courses.component.ts
@@ -10,11 +10,50 @@ import { CoursesService } from "./courses.service";
                     {{ course }}
                 </li>
             </ul>
+
+            <table>
+                <tr>
+                    <td [attr.colspan]="colSpan"></td>
+                </tr>
+            </table>
+
+            <button class="btn btn-primary" [class.active]="isActive"> Button </button>
+            <button [style.backgroundColor]="isActive ? 'blue' : 'white' "> Save </button>
+            
+            <div (click)="onDivClicked()">
+            <button (click)="onSave($event)">Save</button>
+            </div>
+
+            <input [value]="email" (keyup.enter)="onKeyUp();" />
+            <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" /> <br/>
+
+            {{ course.title | uppercase | lowercase }} <br/>
+            {{ course.students | number }} <br/>
+            {{ course.rating | number:'1.1-1' }} <br/>
+            {{ course.price | currency:'AUD':true:'3.2-2' }} <br/>
+            {{ course.releaseDate | date:'shortDate' }} <br/>
+
+            {{ text | summary:10 }}
             `
 })
 export class CoursesComponent {
     title = "List of Courses"
-    
+    colSpan = 2;
+    isActive = true;
+    email = "me@example.com";
+
+    course = {
+        title: "The Complete Angular Course",
+        rating: 4.95734,
+        students: 1231,
+        price: 190.95,
+        releaseDate: new Date(2021, 0, 12)
+    }
+
+    text = `
+        Hello there, lorem ipsum is simply dummy
+    `
+
     getTitle() {
         return this.title;
     }
@@ -25,6 +64,20 @@ export class CoursesComponent {
         this.courses = service.getCourses();
     }
 
+    onSave($event: any) {
+        $event.stopPropagation();
+        console.log("Button was clicked", $event);
+    }
 
+    onDivClicked() {
+        console.log("Div was clicked");
+    }
+
+    onKeyUp() {
+        // if ($event.keyCode === 13)
+            // console.log("ENTER WAS PRESSED");
+        // console.log($event.target.value);
+        console.log(this.email);
+    }
     // Logic for calling an HTTP service
 }
