7570c05d7e9327ad22e470e5badb1717e492f1a5
Cernat <cernatrazvanm@yahoo.com>
2021-12-28 12:05:50 +0200

    Implemented JMS Added a consumer Configured docker-compose to get an instance of ActiveMQ #89

---
+++ library-backend/library-app/src/main/java/org/internship/library/app/jms/MessageConsumer.java
@@ -0,0 +1,28 @@
+package org.internship.library.app.jms;
+
+import org.internship.library.api.dto.BookDTO;
+import org.internship.library.app.adapter.BookMapper;
+import org.internship.library.app.persistence.repository.BookSpringProvidedRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer
+{
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @Autowired
+    BookSpringProvidedRepository bookSpringProvidedRepository;
+
+    @JmsListener(destination = "publishBook")
+    public void message(BookDTO bookDTO)
+    {
+        LOGGER.info("RECEIVED MESSAGE {}", bookDTO);
+
+        bookSpringProvidedRepository.save(BookMapper.bookDTOtoBookEntity(bookDTO));
+    }
+}
